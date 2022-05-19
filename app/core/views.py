dcfecdda9611d85f2b332d52f3a80239903083ae
Renan Santana <renan_ota@hotmail.com>
2022-05-18 23:08:16 -0300

    finaliza view

--- app/core/views.py
+++ app/core/views.py
@@ -6,5 +6,16 @@ from core.models import Upload
 
 def image_upload(request):
     if request.method == "POST":
-        pass
+        image_file = request.FILES["image_file"]
+        image_type = request.POST.get("image_type", False)
+        if settings.USE_S3:
+            upload = Upload(file=image_file)
+            upload.save()
+            image_url = upload.file.url
+        else:
+            fs = FileSystemStorage()
+            filename = fs.save(image_file.name, image_file)
+            image_url = fs.url(filename)
+        return render(request, "upload.html", {"image_url": image_url})
+
     return render(request, "upload.html")
